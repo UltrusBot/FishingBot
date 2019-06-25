6223953ebd9968ae9f9307ec9a829fa0c64fd578
UltraBot <ultraguy55@gmail.com>
2019-06-24 21:02:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-require('dotenv').load();
+require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
 /*To create unique version of bot, add a .env file with the line:
 TOKEN="YOUR_TOKEN_HERE"
 */
-console.log(process.env.TOKEN)
 client.on('ready', () => {
   console.log(`Fishing Bot logged in at ${Date.now()}`);
 });
