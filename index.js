f5716ff7a99ec0b9c9d1ca7dc4410fbe7b5072c8
UltraBot <ultraguy55@gmail.com>
2019-06-25 02:30:03 -0400

    Added command: help

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 require('dotenv').config()
 const Discord = require('discord.js');
+//const sqlite = require("")
 const client = new Discord.Client();
+const prefix = "f!"
+const helpEmbed = new Discord.RichEmbed({
+  "title": "Fishing Bot Commands!",
+  "color": 53380,
+  "fields": [{
+    "name": "**help**",
+    "value": "Shows you this help menu, with all the commands!",
+    "inline": true
+  }]
+})
+;
 /*To create unique version of bot, add a .env file with the line:
 TOKEN="YOUR_TOKEN_HERE"
 */
@@ -9,6 +21,12 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+  if (msg.content.substring(0,  2) != prefix) return;
 
+  let args = msg.content.slice(prefix.length).split(" ");
+  let command = args.shift();
+  if (command == "help") {
+    msg.channel.send(helpEmbed)
+  }
 });
 client.login(process.env.TOKEN);
